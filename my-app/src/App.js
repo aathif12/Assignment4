ae93161caaac3b0e9a2941c7da62bc9bf0c3c390
aathif12 <aathifahamad4@gmail.com>
2024-11-24 20:03:51 +0530

    error Debg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Animal from "../src/components/Animal";
+import { animals } from "./data/AnimalsDb";
+import "../src/assets/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <h1>Animal Matching Game</h1>
+      </div>
+      <div className="container">
+        <Animal animal={animals}></Animal>
+      </div>
     </div>
   );
 }
